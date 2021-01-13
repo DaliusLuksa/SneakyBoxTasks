b4b7cc62d690918164cc36986119353654323822
dalius.luksa1 <dalius.luksa1@gmail.com>
2021-01-13 22:05:03 +0200

    Connected database to the project

---
+++ MyCitiesWeatherForecast/Entities/CitiesDBContext.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.Extensions.Options;
+using MyCitiesWeatherForecast.Helpers;
+
+namespace MyCitiesWeatherForecast.Entities
+{
+    public partial class CitiesDBContext : DbContext
+    {
+        private readonly AppSettings _appSettings;
+
+        public CitiesDBContext()
+        {
+        }
+
+        public CitiesDBContext(DbContextOptions<CitiesDBContext> options, IOptions<AppSettings> appSettings)
+            : base(options)
+        {
+            _appSettings = appSettings.Value;
+        }
+
+        public virtual DbSet<City> City { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseMySql(_appSettings.DefaultConnection);
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<City>(entity =>
+            {
+                entity.ToTable("city");
+
+                entity.Property(e => e.Id).HasColumnType("int(11)");
+
+                entity.Property(e => e.Description)
+                    .HasColumnName("description")
+                    .HasMaxLength(255)
+                    .HasDefaultValueSql("'NULL'");
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasColumnName("name")
+                    .HasMaxLength(255);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ MyCitiesWeatherForecast/Entities/City.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyCitiesWeatherForecast.Entities
+{
+    public partial class City
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
